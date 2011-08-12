8bfa5bb34641cd1e038547fb0b2a4e3278c03814
Francisco de Freitas <favdefreitas@gmail.com>
2011-08-12 15:20:27 +0200

    Initial release

---
+++ akka-microbenchmarking2/src/main/scala/akka_microbench/local/LocalRandomPing.scala
@@ -0,0 +1,134 @@
+/**
+ * @author Francisco de Freitas
+ *
+ * This is a microbenchmark to test Akka
+ *
+ * The algorithm sends 'ping' messages to some initial workers (akka-actors). These than randomly
+ * choose a worker to send another 'ping' message to. At each worker, the message hop is decreased until
+ * it reaches zero, converging the algorithm to terminate.
+ *
+ */
+package akka_microbench.local
+
+import akka.actor.Actor
+import akka.actor.Actor._
+import akka.actor.ActorRef
+import akka.actor.PoisonPill
+import akka.dispatch._
+
+import scala.util.Random
+
+import java.util.Date
+
+import akka.actor.ReceiveTimeout
+
+object LocalRandomPing extends App {
+
+  startPinging(workers = 8, messages = 10000, hops = 1000, repetitions = 5)
+
+  sealed trait PingMessage
+  case class Start extends PingMessage
+  case class Ping(hops: Int) extends PingMessage
+  case class End extends PingMessage
+
+  var workers: Vector[ActorRef] = _
+
+  var runs: List[Long] = List()
+
+  class Worker(coordRef: ActorRef, numWorkers: Int) extends Actor {
+    
+    self.dispatcher = Dispatchers.newThreadBasedDispatcher(self)
+
+    def receive = {
+
+      case Ping(hops) =>
+        if (hops == 0)
+          coordRef ! End
+        else
+          workers(Random.nextInt(numWorkers)) ! Ping(hops - 1)
+
+      case End =>
+        self.stop()
+    }
+
+  }
+
+  class Master(numWorkers: Int, numMessages: Int, numHops: Int, repetitions: Int) extends Actor {
+    
+    self.dispatcher = Dispatchers.newThreadBasedDispatcher(self)
+
+    var start: Long = 0
+    var end: Long = 0
+    var receivedEnds: Int = 0
+    var reps: Int = 1
+
+    def receive = {
+
+      case Start =>
+
+        workers = Vector()
+        receivedEnds = 0
+        // create the workers
+        workers = Vector.fill(numWorkers)(actorOf(new Worker(self, numWorkers)).start())
+
+        println("Master start run #" + reps)
+
+        start = System.nanoTime
+
+        // send to all of the workers some messages
+        for (i <- 0 until numWorkers)
+          for (j <- 0 until numMessages)
+            workers(Random.nextInt(numWorkers)) ! Ping(numHops)
+
+      case End =>
+        receivedEnds += 1
+
+        // all messages have reached 0 hops
+        if (receivedEnds == numWorkers * numMessages) {
+          end = System.nanoTime
+
+          println("Run #" + reps + " ended! Time = " + ((end - start) / 1000000.0) + "ms")
+
+          runs = (end - start) :: runs
+
+          if (reps != repetitions) {
+            reps += 1
+            self ! Start
+          } else {
+            println("Repetitions reached. Broadcasting shutdown...")
+            workers.foreach { x => x ! PoisonPill }
+            self.stop()
+          }
+        }
+
+    }
+
+    override def preStart {
+      println("Start pinging around: " + new Date(System.currentTimeMillis))
+    }
+
+    override def postStop {
+      println("End: " + new Date(System.currentTimeMillis))
+      val avg = runs.foldLeft(0l)(_ + _) / runs.size
+      println("Average execution time = " + avg / 1000000.0 + " ms")
+      System.exit(0)
+    }
+
+  }
+
+  def startPinging(workers: Int, messages: Int, hops: Int, repetitions: Int) {
+
+    println("Workers: " + workers)
+    println("Messages: " + messages)
+    println("Hops: " + hops)
+    println("Repetitions: " + repetitions)
+
+    // create the master
+    val coordRef = actorOf(new Master(workers, messages, hops, repetitions)).start()
+
+    // start the calculation
+    coordRef ! Start
+
+  }
+
+}
---
+++ akka-microbenchmarking2/src/main/scala/akka_microbench/remote/RemoteRandomPing.scala
@@ -0,0 +1,206 @@
+/**
+ * @author Francisco de Freitas
+ *
+ * This is a microbenchmark to test Akka
+ *
+ * The algorithm sends 'ping' messages to some initial workers (akka-actors). These than randomly
+ * choose a worker to send another 'ping' message to. At each worker, the message hop is decreased until
+ * it reaches zero, converging the algorithm to terminate.
+ *
+ *
+ * USAGE: (akka.conf used provided in root folder)
+ *
+ * Define IPs, start machine 1 first then start machine 0. Machine 1 is the slave, machine 0 is the master
+ * 
+ * Machine 0 is where the master worker stays and is the one collecting results.
+ * 
+ * Both machines have 4 workers each, summing a total of 8 workers
+ * 
+ *
+ */
+package akka_microbench.remote
+
+import akka.actor.Actor
+import Actor._
+import akka.actor.ActorRef
+import akka.actor.PoisonPill
+import akka.actor.ReceiveTimeout
+import akka.dispatch._
+
+import java.util.Date
+import java.net.InetAddress
+
+import scala.util.Random
+
+sealed trait PingMessage
+case object Start extends PingMessage
+case class Ping(hops: Int) extends PingMessage
+case object End extends PingMessage
+
+trait MyActor extends Actor {
+
+  def machine0 = "130.60.157.52"
+
+  def machine1 = "130.60.157.139"
+
+}
+
+class Worker(coordRef: ActorRef, numWorkers: Int) extends MyActor {
+
+  self.dispatcher = Dispatchers.newThreadBasedDispatcher(self)
+
+  var workers: Array[ActorRef] = new Array[ActorRef](numWorkers)
+
+  override def preStart {
+
+    for (i <- 0 until 4)
+      workers(i) = remote.actorFor("worker-service" + i, machine0, 2552)
+
+    for (i <- 4 until 8)
+      workers(i) = remote.actorFor("worker-service" + i, machine1, 2552)
+
+  }
+
+  def receive = {
+
+    case Ping(hops) =>
+      if (hops == 0)
+        coordRef ! End
+      else
+        workers(Random.nextInt(numWorkers)) ! Ping(hops - 1)
+
+    case End =>
+      self.stop()
+  }
+
+}
+
+class Master(numWorkers: Int, numMessages: Int, numHops: Int, repetitions: Int) extends MyActor {
+
+  self.dispatcher = Dispatchers.newThreadBasedDispatcher(self)
+
+  var start: Long = 0
+  var end: Long = 0
+  var receivedEnds: Int = 0
+  var reps: Int = 1
+
+  var runs: List[Long] = List()
+  var workers: Array[ActorRef] = new Array[ActorRef](numWorkers)
+
+  def receive = {
+
+    case Start =>
+
+      receivedEnds = 0
+
+      // create the workers
+      for (i <- 0 until 4)
+        workers(i) = remote.actorFor("worker-service" + i, machine0, 2552)
+
+      for (i <- 4 until 8)
+        workers(i) = remote.actorFor("worker-service" + i, machine1, 2552)
+
+      println("Master start run #" + reps)
+
+      start = System.nanoTime
+
+      // send to all of the workers 'numMessages' messages
+      for (i <- 0 until numWorkers)
+        for (j <- 0 until numMessages)
+          workers(i) ! Ping(numHops)
+
+    case End =>
+      receivedEnds += 1
+
+      // all messages have reached 0 hops
+      if (receivedEnds == numWorkers * numMessages) {
+        end = System.nanoTime
+
+        println("Run #" + reps + " ended! Time = " + ((end - start) / 1000000.0) + "ms")
+
+        runs = (end - start) :: runs
+
+        if (reps != repetitions) {
+          reps += 1
+          self ! Start
+        } else {
+          println("Repetitions reached. Broadcasting shutdown...")
+          workers.foreach { x => x ! PoisonPill }
+          self.stop()
+        }
+      }
+
+  }
+
+  override def preStart {
+    println("Start pinging around @ " + new Date(System.currentTimeMillis))
+  }
+
+  override def postStop {
+    println("End: " + new Date(System.currentTimeMillis))
+    val avg = runs.foldLeft(0l)(_ + _) / runs.size
+    println("Average execution time = " + avg / 1000000.0 + " ms")
+    System.exit(0)
+  }
+
+}
+
+object RemoteRandomPingMachine0 {
+
+  val machine0 = "130.60.157.52"
+
+  def main(args: Array[String]): Unit = {
+
+    val numWorkersTotal = 8
+
+    remote.start(InetAddress.getLocalHost.getHostAddress, 2552)
+
+    val coord = remote.actorFor("coord-service", machine0, 2552)
+
+    remote.register("worker-service0", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service1", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service2", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service3", actorOf(new Worker(coord, numWorkersTotal)))
+
+    val workers = 8
+    val messages = 10000
+    val hops = 100
+    val repetitions = 5
+
+    println("Workers: " + workers)
+    println("Messages: " + messages)
+    println("Hops: " + hops)
+    println("Repetitions: " + repetitions)
+
+    // create the master
+    remote.register("coord-service", actorOf(new Master(workers, messages, hops, repetitions)))
+
+    val coordRef = remote.actorFor("coord-service", machine0, 2552)
+
+    // start the calculation
+    coordRef ! Start
+
+  }
+
+}
+
+object RemoteRandomPingMachine1 {
+
+  val machine1 = "130.60.157.139"
+
+  def main(args: Array[String]): Unit = {
+
+    val numWorkersTotal = 8
+
+    remote.start(InetAddress.getLocalHost.getHostAddress, 2552)
+
+    val coord = remote.actorFor("coord-service", machine1, 2552)
+
+    remote.register("worker-service4", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service5", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service6", actorOf(new Worker(coord, numWorkersTotal)))
+    remote.register("worker-service7", actorOf(new Worker(coord, numWorkersTotal)))
+
+  }
+
+}
